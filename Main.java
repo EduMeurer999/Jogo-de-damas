8636b7364bdbcd601e8fe778a3c037e5e72d17a5
Eduardo <eduardomeurer999@gmail.com>
2020-09-29 00:33:35 -0300

    Renderização do tabuleiro; Finalizada

---
+++ Main.java
@@ -0,0 +1,16 @@
+import Tabuleiro.Tabuleiro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tabuleiro tab = new Tabuleiro();
+        String[][] t = new String[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                
+            }
+        }
+        System.out.println(tab.desenhaTabuleiro(t, true));
+
+    }
+}
---
+++ Tabuleiro/Tabuleiro.java
@@ -0,0 +1,56 @@
+package Tabuleiro;
+
+public class Tabuleiro {
+    private String[][] tab = new String[8][8];
+    
+    public Tabuleiro(){
+        
+    }
+    public String desenhaTabuleiro(String[][] tab, boolean emBranco){
+        String strTabuleiro = "";
+        if(emBranco){
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if(i == 0 && j == 0){
+                        strTabuleiro += "\n | a | b | c | d | e | f | g | h |\n";
+                        strTabuleiro += " |---|---|---|---|---|---|---|---|\n";
+                    }
+                    if(j == 0){
+                        strTabuleiro += ""+(i+1);
+                    }
+                    
+                    strTabuleiro += "|   ";
+                    if(j == 7){
+                        strTabuleiro += "|\n |---|---|---|---|---|---|---|---|\n";
+                    }
+                    
+                }
+            }
+        }       
+        else{
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if(i == 0 && j == 0){
+                        strTabuleiro += "\n | a | b | c | d | e | f | g | h |\n";
+                        strTabuleiro +=   " |---|---|---|---|---|---|---|---|\n";
+                    }
+                    if(j == 0){
+                        strTabuleiro += ""+(i+1);
+                    }
+                    if(tab[i][j] == null){
+                        strTabuleiro += "|   ";
+                    }else{
+                        strTabuleiro += "|  "+tab[i][j]+" ";
+                    }
+                    
+                    if(j == 7){
+                        strTabuleiro += "|\n |---|---|---|---|---|---|---|---|\n";
+                    }
+                    
+                }
+            } 
+        }
+        return strTabuleiro;
+    }
+
+}
